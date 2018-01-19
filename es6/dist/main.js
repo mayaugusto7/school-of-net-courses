ed42e2a87d68b51249fe7ad96576409acd447501
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2018-01-19 15:48:32 -0200

    finish course es6

---
+++ es6/dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("hello".indexOf("ello") === 1);
+
+console.log("hello".startsWith("ello", 1));
+console.log("hello".endsWith("ello", 4));
+console.log("hello".includes("hello", 1)); // esta dentro da string
