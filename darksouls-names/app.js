369d1d4a51daa4992394a83a716206499ff94c59
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2018-01-31 15:56:26 -0200

    novo curso de layouts bootstrap

---
+++ darksouls-names/app.js
@@ -0,0 +1,7 @@
+var names = require('darksouls-names');
+
+console.log(names.all());
+console.log(names.random('I', 'Bosses'));
+console.log(names.random('II', 'NPCs'));
+console.log(names.random('III', 'Enemies'));
+console.log(names.random('III', 'Enemies', 3));
